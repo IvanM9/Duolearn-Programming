cfd5579eca6fa5def6f3f7d5b62226478507b84a
Iván Manzaba <66442822+IvanM9@users.noreply.github.com>
2022-02-12 23:09:01 -0500

    correción de los metodos del usuario y se agregó metodos para las actividades (#11)

--- Server/Routes/routes.js
+++ Server/Routes/routes.js
@@ -1,6 +1,7 @@
-const {Router}= require("express");
+const { Router } = require("express");
+const actividades = require("../Controllers/actividades_controller");
 const { estadisticas } = require("../Controllers/estadisticas_controller");
-const {user} = require("../Controllers/usuario_controller");
+const { user } = require("../Controllers/usuario_controller");
 const router = Router();
 
 //usuarios
@@ -11,7 +12,17 @@ router.put("/usuario/modificar", user.modificarUsuario);
 router.post("/iniciar_sesion", user.iniciarSesion);
 router.get("/cerrar_sesion", user.cerrarSesion);
 router.delete('/usuario/eliminar', user.elimnarUsuario);
+router.post("/recuperar_clave", user.solicitarClave)
+
+
+//actividades
+router.get("/actividades/obtener", actividades.obtenerActividades);
+router.post("/actividades/resolver", actividades.resolverActividad);
+
 
 //estadisticas
 router.get("/usuario/estadisticas_java", estadisticas.getJava);
+
+
+
 module.exports = router;
