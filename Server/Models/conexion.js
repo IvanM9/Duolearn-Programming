32c6d903ee46a9628cea2e21d7023f1bb5889b26
Iván Manzaba <mauricio.9.inm@gmail.com>
2022-03-01 18:33:43 -0500

    deploy app

--- Server/Models/conexion.js
+++ Server/Models/conexion.js
@@ -1,15 +1,35 @@
-const {Pool} = require("pg");
+const { Pool } = require("pg");
+const url = require('url');
 require("dotenv").config();
 
 //Configuración de la conexión a la base de datos
-var pool = new Pool({
-    user: process.env.USERDB,
-    host: process.env.HOST,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: '5432'
+var config;
+if (process.env.DATABASE_URL) {
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
 
-})
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: {
+            rejectUnauthorized: false
+        }
+    };
+}
+else {
+    config = {
+        user: process.env.USERDB,
+        host: process.env.HOST,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        port: '5432'
+    }
+}
+console.log(config);
+var pool = new Pool(config)
 
 
-module.exports = {pool};
+module.exports = { pool };
