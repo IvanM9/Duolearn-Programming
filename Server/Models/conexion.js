9b6f362fa35353467bc9ca3abb60dd7fabfa4650
Iván Manzaba <mauricio.9.inm@gmail.com>
2022-03-01 14:42:18 -0500

    deploy servidor

--- Server/Models/conexion.js
+++ Server/Models/conexion.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 //Configuración de la conexión a la base de datos
 var pool = new Pool({
     user: process.env.USERDB,
-    host: 'localhost',
+    host: process.env.HOST,
     password: process.env.PASSWORD,
-    database: 'DuolearnP',
+    database: process.env.DATABASE,
     port: '5432'
 
 })
